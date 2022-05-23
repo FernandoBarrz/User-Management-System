cfb13dd4eaf06a432991a19b77cfe879e5a29c63
Fernando Barrios <fernando.barrios.dev@gmail.com>
2022-05-23 09:50:06 -0500

    Added secure DB credentials

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const closeModaleHTML = document.querySelector('.closemodale');
 const modaleHTML = document.querySelector('.modale');
 
 
-
-
 openModaleHTML.addEventListener("click", (e)=>{
     e.preventDefault();
     modaleHTML.classList.add('opened');
@@ -17,12 +15,16 @@ closeModaleHTML.addEventListener("click", (e)=>{
     modaleHTML.classList.remove('opened');
 });
 
-// $('.openmodale').click(function (e) {
-//     e.preventDefault();
-//     $('.modale').addClass('opened');
-// });
-
-// $('.closemodale').click(function (e) {
-//     e.preventDefault();
-//     $('.modale').removeClass('opened');
-// });
+function search(){
+    var resultado = [
+        {
+            "address": "Condesa #56",
+            "email": "PABLP@gmail.com",
+            "firstname": "Pablo",
+            "id": 4,
+            "lastname": "Perez",
+            "phone": "67567"
+        }
+    ]
+    return resultado
+}
